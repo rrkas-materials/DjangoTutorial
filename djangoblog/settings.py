4f4e84386b7e86405b04e554704d8740cc2f6cdd
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-29 14:52:02 +0530

    Profile and images

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -8,17 +9,14 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} successfully!')
-            return redirect('blog-home')
+            # username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully! You are now able to login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
-
-# messages.debug
-# messages.info
-# messages.success
-# messages.warning
-# messages.error
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
