f81b3dc9ee43f2a880500d34ef2169d784daeb63
Rohnak Agarwal <rrka79wal@gmail.com>
2021-06-06 09:25:24 +0530

    pagination, password reset

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import *
 from django.views.generic import *
 from .models import Post
+from django.contrib.auth.models import User
 
 
 # default template name for class based view: <app-name>/<model-name>_<view-type>.html
@@ -12,6 +13,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 # def home(request):
